32be96edd2387c80a5003b884cb756505e1dc99d
Arun S <arunsangubhatla@gmail.com>
2020-08-24 16:50:55 -0400

    Update SignPASlaveCertOnMaster.sh

--- SignPASlaveCertOnMaster.sh
+++ SignPASlaveCertOnMaster.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 # script to sign puppet slave cert on master through jenkins job
-sudo cd /opt/puppetlabs/bin/
-sudo puppet cert list --all
-sudo puppet cert sign --all
+/opt/puppetlabs/bin/sudo puppet cert list --all
+/opt/puppetlabs/bin/sudo puppet cert sign --all
