e9b3dbdd2c173633db458a87e9ec40ddf549aa40
Arun S <arunsangubhatla@gmail.com>
2020-08-26 16:57:41 -0400

    Update ansible_chrome_browserNdriver.sh

--- ansible_chrome_browserNdriver.sh
+++ ansible_chrome_browserNdriver.sh
@@ -2,7 +2,7 @@
 
 # Commands to get/install chromium browser 
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm
-yum install google-chrome-stable_current_x86_64.rpm
+yum install google-chrome-stable_current_x86_64.rpm -y
 
 # Commands to get/install chrome driver 
 wget -N http://chromedriver.storage.googleapis.com/2.40/chromedriver_linux64.zip -P /usr/tmp
