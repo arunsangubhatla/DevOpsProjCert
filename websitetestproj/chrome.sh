c36fe1538a18e258d5364ad67046957c16457444
Arun Sangubhatla <arunsangubhatla@gmail.com>
2020-08-26 19:29:30 -0400

    shell script changes

---
+++ websitetestproj/chrome.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Commands to get/install chromium browser 
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm
+yum install google-chrome-stable_current_x86_64.rpm -y
+
+# Commands to get/install chrome driver 
+wget -N http://chromedriver.storage.googleapis.com/2.40/chromedriver_linux64.zip -P /usr/tmp
+unzip /usr/tmp/chromedriver_linux64.zip -d /usr/tmp
+mv -f /usr/tmp/chromedriver /usr/local/share/
+chmod +x /usr/local/share/chromedriver
+ln -s /usr/local/share/chromedriver /usr/local/bin/chromedriver
+ln -s /usr/local/share/chromedriver /usr/bin/chromedriver
--- websitetestproj/src/test/java/TestNG/tc1.java
+++ websitetestproj/src/test/java/TestNG/tc1.java
@@ -15,25 +15,18 @@ public class tc1 {
 		@BeforeMethod
 		public void launch() {
 		  System.setProperty("webdriver.chrome.driver","chromedriver");
-		  //driver = new ChromeDriver();
 		  ChromeOptions chromeOptions = new ChromeOptions();
-		  
 		  chromeOptions.addArguments("--headless");
-		   
 		  chromeOptions.addArguments("--no-sandbox");
-		   
 		  driver = new ChromeDriver(chromeOptions);
-		   
 		  chromeOptions.addArguments("--headless");
 		  driver.manage().window().maximize();
 		  driver.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS);
-		  driver.get("http://localhost/index.php/");
-		
+		  driver.get("http://localhost/index.php/");	
 		}
 		
 	  @Test
-	  public void verfiy() {
-		 
+	  public void verfiy() {		 
 		  driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
 		  driver.findElement(By.id("About Us")).click(); 
 		  driver.findElement(By.id("PID-ab2-pg")).getText().equals("This is about page. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.");
