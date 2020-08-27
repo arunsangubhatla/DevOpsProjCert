f14f16ecdb777d7076fabf21e426057cfc8175f5
Arun S <arunsangubhatla@gmail.com>
2020-08-27 19:40:43 -0400

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -10,5 +10,9 @@ ADD website /var/www/html
 RUN rm /var/www/html/index.html
 CMD apachectl -D FOREGROUND
 EOT
-docker build -t devopscert:$BUILD_NUMBER .
-docker run -itd -p devopscert:$BUILD_NUMBER
+docker ps -q --filter ancestor="devopscert:latest" | xargs docker stop
+docker image tag myimage:latest devopscert:previous
+docker build -t devopscert:latest .
+docker run -itd -p 8081:8080 devopscert:latest
+#docker build -t devopscert:$BUILD_NUMBER .
+#docker run -itd -P devopscert:$BUILD_NUMBER
